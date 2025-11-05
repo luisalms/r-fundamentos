9f88a662a37864e81eb619e52895312705957f09
lluisalinhares <lluisalinhares@gmail.com>
2025-11-05 12:55:44 -0300

    primeiros passos em R; variáveis

---
+++ introducao.R
@@ -0,0 +1,22 @@
+# Introdução ao RStudio e as Variáveis
+
+# Para executar os comandos: ctrl + Enter. 
+# Também é possível selecionar um conjunto de linhas e executar todas juntas
+
+# Para limpar o console: ctrl + L
+
+# Para atribuir funções: operador de igualdade =
+
+# Para declarar variáveis: operador seta para esquerda <-
+# Exemplos:
+
+a <- 10
+b <- 10
+c <- a + b
+
+print(c)
+
+# Strings:
+
+mensagem <- "Hello, World!"
+print(mensagem)

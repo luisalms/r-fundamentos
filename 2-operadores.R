de2d025606511b895baa9177adb8ca91e880c440
lluisalinhares <lluisalinhares@gmail.com>
2025-11-14 22:42:18 -0300

    operadores em R

---
+++ 2-operadores.R
@@ -0,0 +1,82 @@
+# Operadores
+
+# Operações básicas:
+
+# soma:
+5 + 5
+
+# subtração:
+10 - 5
+
+# divisão:
+9/3
+
+# multiplicação
+4 * 2
+
+# potência:
+4 ** 2
+4 ^ 2
+
+
+# comparações:
+
+# igual:
+5 == 6
+5 == 5
+
+# diferente:
+5 != 6
+5 != 5
+
+# maior: 
+5 > 5
+6 > 5
+
+# menor:
+5 < 5
+4 < 5
+
+# maior ou igual:
+5 >= 5
+5 >= 6
+
+# menor ou igual:
+5 <= 5
+5 <= 4
+
+
+# Operadores aritméticos:
+
+# módulo (resto da divisão):
+10 %% 3
+
+# divisão inteira:
+10 %/% 3
+
+
+# Operadores lógicos:
+
+# AND (&) - Ambos devem ser TRUE
+5 > 3 & 10 < 20  # TRUE & TRUE = TRUE
+5 > 3 & 10 > 20  # TRUE & FALSE = FALSE
+
+# OR (|) - Pelo menos um TRUE
+5 > 3 | 10 > 20  # TRUE | FALSE = TRUE
+5 < 3 | 10 > 20  # FALSE | FALSE = FALSO
+
+# NOT (!) - Inverte o resultado
+!TRUE # FALSE
+
+!(5 > 3)  # !TRUE = FALSE
+!(5 < 3)  # !FALSE = TRUE
+
+
+# Operadores de atribuição:
+
+x <- 10
+y = 20
+30 -> z
+
+
+
